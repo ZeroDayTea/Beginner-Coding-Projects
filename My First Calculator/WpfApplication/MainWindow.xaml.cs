3b594be672fef4cb38ca50fb49b8f0b1fbe4460e
Patrick Dobranowski <45400957+ZeroDayTea@users.noreply.github.com>
2021-04-19 12:31:33 -0600

    Add files via upload

---
+++ My First Calculator/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();  
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            double Input1, Input2;
+
+            try
+            {
+                Input1 = Convert.ToDouble(txtInput1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Input1: " + ex.Message, "Error");
+                return;
+            }
+
+            try
+            {
+                Input2 = Convert.ToDouble(txtInput2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Input2: " + ex.Message, "Error");
+                return;
+            }
+
+            txtResult.Text = (Input1 + Input2).ToString();
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            double Input1, Input2;
+
+            try
+            {
+                Input1 = Convert.ToDouble(txtInput1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Input1: " + ex.Message, "Error");
+                return;
+            }
+
+            try
+            {
+                Input2 = Convert.ToDouble(txtInput2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Input2: " + ex.Message, "Error");
+                return;
+            }
+
+            txtResult.Text = (Input1 - Input2).ToString();
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            double Input1, Input2;
+
+            try
+            {
+                Input1 = Convert.ToDouble(txtInput1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Input1: " + ex.Message, "Error");
+                return;
+            }
+
+            try
+            {
+                Input2 = Convert.ToDouble(txtInput2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Input2: " + ex.Message, "Error");
+                return;
+            }
+
+            txtResult.Text = (Input1 * Input2).ToString();
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            double Input1, Input2;
+
+            try
+            {
+                Input1 = Convert.ToDouble(txtInput1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Input1: " + ex.Message, "Error");
+                return;
+            }
+
+            try
+            {
+                Input2 = Convert.ToDouble(txtInput2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Input2: " + ex.Message, "Error");
+                return;
+            }
+
+            txtResult.Text = (Input1 / Input2).ToString();
+        }
+
+    }
+}
