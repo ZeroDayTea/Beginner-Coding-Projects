26f4580f48cb0a48a9ad029b21c155116b96aa0f
Patrick Dobranowski <45400957+ZeroDayTea@users.noreply.github.com>
2021-04-19 12:30:51 -0600

    Add files via upload

---
+++ HelloWorld/HelloWorld/FirstWindows.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace HelloWorld
+{
+    partial class FirstWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHello = new System.Windows.Forms.TextBox();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHello
+            // 
+            this.txtHello.Location = new System.Drawing.Point(39, 46);
+            this.txtHello.Name = "txtHello";
+            this.txtHello.Size = new System.Drawing.Size(278, 22);
+            this.txtHello.TabIndex = 0;
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.Location = new System.Drawing.Point(36, 81);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(281, 17);
+            this.lblHelloWorld.TabIndex = 1;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(208, 150);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(99, 27);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // FirstWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 206);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.txtHello);
+            this.Name = "FirstWindow";
+            this.Text = "HelloWorld";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHello;
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
